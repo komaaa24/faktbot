65b025e6833685a31378d93b4a21b57c7d7a2b33
kamol666 <ktoshpulatov17@gmail.com>
2026-01-29 18:25:49 +0500

    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,10 @@ module.exports = {
     apps: [
         {
             name: 'biznes-goyalar-bot',
+            cwd: __dirname,
             script: 'dist/main.js',
             node_args: ['-r', 'dotenv/config'],
+            exec_mode: 'fork',
             instances: 1,
             autorestart: true,
             watch: false,
